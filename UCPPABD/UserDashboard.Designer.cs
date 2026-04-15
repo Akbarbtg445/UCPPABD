a1c2eaace8194bbfef97e85aee6db23b50ac9216
M.Ridho Akbar <akbarbtg445@gmail.com>
2026-04-16 06:43:23 +0700

    membuat log out user

--- UCPPABD/UserDashboard.Designer.cs
+++ UCPPABD/UserDashboard.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(811, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOG OUT";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // UserDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(956, 450);
+            this.ClientSize = new System.Drawing.Size(956, 578);
+            this.Controls.Add(this.button1);
             this.Name = "UserDashboard";
             this.Text = "UserDashboard";
             this.ResumeLayout(false);
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
